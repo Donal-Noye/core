0212bc78a2d00ef8a504e4614ae6d57471bafdbb
donalnoye <leekeiz49@gmail.com>
2025-04-23 16:43:02 +0400

    add course list

---
+++ src/features/courses-list/model/types.ts
@@ -0,0 +1,15 @@
+type CourseListElement = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+type CreateCourseListElementCommand = {
+  name: string
+  description: string
+}
+
+type DeleteCourseListElementCommand = {
+  id: string
+}
+
