5aa2fa4d0534c905cbeb572103cfdeff63c55e77
donalnoye <leekeiz49@gmail.com>
2025-05-06 02:37:56 +0400

    feat: add check user email in auth

--- src/entities/user/_repositories/user.ts
+++ src/entities/user/_repositories/user.ts
@@ -2,6 +2,12 @@ import { dbClient } from "@/shared/lib/db";
 import { UserEntity, UserId } from "../_domain/types";
 
 export class UserRepository {
+  async getByEmail(email: string): Promise<UserEntity | null> {
+    return dbClient.user.findUnique({
+      where: { email },
+    });
+  }
+
   async getUserById(userId: UserId): Promise<UserEntity> {
     return dbClient.user.findUniqueOrThrow({
       where: { id: userId },
