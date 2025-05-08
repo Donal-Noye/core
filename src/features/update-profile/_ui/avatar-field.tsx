82d1519982191b47eefc52a5d3c83a586c7b34b9
donalnoye <leekeiz49@gmail.com>
2025-05-08 23:26:25 +0400

    feat: add upload file action

--- src/features/update-profile/_ui/avatar-field.tsx
+++ src/features/update-profile/_ui/avatar-field.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/shared/ui/button";
 import { Spinner } from "@/shared/ui/spinner";
 import { ProfileAvatar } from "@/entities/user/profile";
+import { useUploadAvatar } from "@/features/update-profile/_vm/use-upload-avatar";
 
 export function AvatarField({
   value,
@@ -9,13 +10,16 @@ export function AvatarField({
   value?: string;
   onChange: (value?: string) => void;
 }) {
+  const { isPending, handleSelectFile } = useUploadAvatar({});
+
   return (
     <Button
       variant="ghost"
       className="w-[84px] h-[84px] p-0.5 rounded-full relative block"
+      onClick={handleSelectFile}
       type="button"
     >
-      {false && (
+      {isPending && (
         <div className="inset-0 absolute flex items-center justify-center z-10">
           <Spinner className="w-10 h-10" aria-label="Загрузка новой аватарки" />
         </div>
